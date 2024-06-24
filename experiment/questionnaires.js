99648364df0da230a2c311aae04f712ae2df9e04
Faris Al-kadi <fa514@sussex.ac.uk>
2024-06-24 16:29:25 +0100

    Demographics edit

--- experiment/questionnaires.js
+++ experiment/questionnaires.js
@@ -30,31 +30,146 @@ var demographics_consent = {
     data: { screen: "consent", condition: "Awe" },
 }
 var demographic_questionnaire = {
-    type: jsPsychSurveyMultiChoice,
-    questions: [
-        {
-            prompt: "What is your age?",
-            options: ["Under 18", "18-24", "25-34", "35-44", "45-54", "55-64", "65 or older"],
-            required: true,
-        },
-        {
-            prompt: "What is your gender?",
-            options: ["Male", "Female", "Other", "Prefer not to say"],
-            required: true,
-        },
-        {
-            prompt: "What is your highest level of education?",
-            options: [
-                "High school or less",
-                "Some college",
-                "Bachelor's degree",
-                "Master's degree",
-                "Doctorate",
-                "Other",
-            ],
-            required: true,
-        },
-    ],
+    type: jsPsychSurvey,
+    survey_json: {
+        title: "About yourself",
+        // description:
+        //     "Please answer the following questions based on how accurately each statement describes you in general.",
+        completeText: "Continue",
+        showQuestionNumbers: false,
+        // goNextPageAutomatic: true,
+        pageNextText: "Next",
+        pagePrevText: "Previous",
+        showProgressBar: "aboveHeader",
+        pages: [
+            {
+                name: "page1",
+                elements: [
+                    {
+                        title: "What is your gender?",
+                        name: "Gender",
+                        type: "radiogroup",
+                        choices: ["Male", "Female", "Other"],
+                        isRequired: true,
+                        colCount: 0,
+                    },
+                    {
+                        type: "text",
+                        title: "Please enter your age (in years)",
+                        name: "Age",
+                        isRequired: true,
+                        inputType: "number",
+                        min: 0,
+                        max: 100,
+                        // defaultValue: 0,
+                    },
+                ],
+            },
+            {
+                elements: [
+                    {
+                        title: "What is your highest completed education level?",
+                        name: "Education",
+                        type: "radiogroup",
+                        choices: [
+                            {
+                                value: "Doctorate",
+                                text: "University (doctorate)",
+                            },
+                            {
+                                value: "Master",
+                                text: "University (master)", // "<sub><sup>or equivalent</sup></sub>",
+                            },
+                            {
+                                value: "Bachelor",
+                                text: "University (bachelor)", // "<sub><sup>or equivalent</sup></sub>",
+                            },
+                            {
+                                value: "High school",
+                                text: "High school",
+                            },
+                            {
+                                value: "Elementary school",
+                                text: "Elementary school",
+                            },
+                        ],
+                        showOtherItem: true,
+                        otherText: "Other",
+                        otherPlaceholder: "Please specify",
+                        isRequired: true,
+                        colCount: 1,
+                    },
+                    {
+                        visibleIf:
+                            "{Education} == 'Doctorate' || {Education} == 'Master' || {Education} == 'Bachelor'",
+                        title: "What is your discipline?",
+                        name: "Discipline",
+                        type: "radiogroup",
+                        choices: [
+                            "Arts and Humanities",
+                            "Literature, Languages",
+                            "History, Archaeology",
+                            "Sociology, Anthropology",
+                            "Political Science, Law",
+                            "Business, Economics",
+                            "Psychology, Neuroscience",
+                            "Medicine",
+                            "Biology, Chemistry, Physics",
+                            "Mathematics, Physics",
+                            "Engineering, Computer Science",
+                        ],
+                        showOtherItem: true,
+                        otherText: "Other",
+                        otherPlaceholder: "Please specify",
+                    },
+                    {
+                        visibleIf:
+                            "{Education} == 'High school' || {Education} == 'Master' || {Education} == 'Bachelor'",
+                        title: "Are you currrently a student?",
+                        name: "Student",
+                        type: "boolean",
+                        swapOrder: true,
+                        isRequired: true,
+                    },
+                ],
+            },
+            {
+                elements: [
+                    {
+                        title: "How would you describe your ethnicity?",
+                        name: "Ethnicity",
+                        type: "radiogroup",
+                        choices: [
+                            "White",
+                            "Black",
+                            "Hispanic/Latino",
+                            "Middle Eastern/North African",
+                            "South Asian",
+                            "East Asian",
+                            "Southeast Asian",
+                            "Mixed",
+                            "Prefer not to say",
+                        ],
+                        showOtherItem: true,
+                        otherText: "Other",
+                        otherPlaceholder: "Please specify",
+                        isRequired: false,
+                        colCount: 1,
+                    },
+                    {
+                        title: "In which country are you currently living?",
+                        name: "Country",
+                        type: "text",
+                        placeholder: "e.g., France",
+                        isRequired: false,
+                    },
+                ],
+            },
+        ],
+    },
+    data: {
+        screen: "demographic_questions",
+    },
 }
 
 var demographics_waitdatasaving = {
